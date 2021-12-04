95bd67cb725eddb52b5a741acff1e40aa62608bd
Wei Ding <ding_web@yahoo.co.jp>
2021-12-04 15:07:42 -0800

    edit style

--- Times/DualTimers/Timing.cs
+++ Times/DualTimers/Timing.cs
@@ -4,38 +4,38 @@ using System.Timers;
 namespace DualTimers {
 	public class Timing {
 
-		public void Start() {
+		public void Start () {
 			Console.ForegroundColor = ConsoleColor.Green;
-			Console.WriteLine("Timing Class");
+			Console.WriteLine ("Timing Class");
 
-			Timer timer1 = new Timer(5000);
+			Timer timer1 = new (5000);
 			timer1.Elapsed += ONTimer1;
 			timer1.AutoReset = true;
 			timer1.Enabled = true;
 
-			Timer timer2 = new Timer(3000);
+			Timer timer2 = new (3000);
 			timer2.Elapsed += OnTimer2;
 			timer2.AutoReset = true;
 			timer2.Enabled = true;
 
-			Console.WriteLine("Press Enter key to stop the timer service");
-			Console.ReadLine();
+			Console.WriteLine ("Press Enter key to stop the timer service");
+			Console.ReadLine ();
 
-			timer1.Stop();
-			timer1.Dispose();
+			timer1.Stop ();
+			timer1.Dispose ();
 
-			timer2.Stop();
-			timer2.Dispose();
+			timer2.Stop ();
+			timer2.Dispose ();
 		}
 
-		private void ONTimer1(object source, ElapsedEventArgs e) {
+		private void ONTimer1 (object source, ElapsedEventArgs e) {
 			Console.ForegroundColor = ConsoleColor.Cyan;
-			Console.WriteLine("-- Timer 1: " + e.SignalTime);
+			Console.WriteLine ("-- Timer 1: " + e.SignalTime);
 		}
 
-		private void OnTimer2(object source, ElapsedEventArgs e) {
+		private void OnTimer2 (object source, ElapsedEventArgs e) {
 			Console.ForegroundColor = ConsoleColor.Yellow;
-			Console.WriteLine("-- Timer 2: " + e.SignalTime);
+			Console.WriteLine ("-- Timer 2: " + e.SignalTime);
 		}
 	}
 }
