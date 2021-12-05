7e81058eefa031607dea2b06cd04cae2dd3583fb
Wei Ding <ding_web@yahoo.co.jp>
2021-12-05 00:53:55 -0800

    remove unusing namespaces

--- Communications/SignalRChat/Hubs/ChatHub.cs
+++ Communications/SignalRChat/Hubs/ChatHub.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Microsoft.AspNetCore.SignalR;
 using System.Threading.Tasks;
-
-using Microsoft.AspNetCore.SignalR;
 
 namespace SignalRChat.Hubs
 {
